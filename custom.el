2f04ff94f07c3ad3257c30f60a2181ecce4f8f5f
gramps <richmoyer@gmail.com>
2022-11-10 06:31:59 -0600

    Add files via upload

---
+++ custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("849a2cbd01525a48ad263bba281a1918c83c4f87c17e833cf506b572e501a967" "d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" default))
+ '(package-selected-packages '(Emacs-Pyte-theme))
+ '(send-mail-function 'mailclient-send-it)
+ '(warning-suppress-types '((lsp-mode) (defvaralias))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
